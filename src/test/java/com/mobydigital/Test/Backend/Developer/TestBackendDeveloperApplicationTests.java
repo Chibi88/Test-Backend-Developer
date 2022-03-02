d9d14bc352aa1c783387c07e93905464351e3744
Silvina Colonia <silvinacolonia@gmmail.com>
2022-03-02 10:29:18 -0300

    Creación del proyecto

---
+++ src/test/java/com/mobydigital/Test/Backend/Developer/TestBackendDeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobydigital.Test.Backend.Developer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBackendDeveloperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
